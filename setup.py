25e1806e0a2ff6137547036f244258d6a24281f0
do.tranngoc <dotrann.1412@gmail.com>
2024-06-30 23:52:24 +0700

    update

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ def get_extensions():
             "-D__CUDA_NO_HALF_OPERATORS__",
             "-D__CUDA_NO_HALF_CONVERSIONS__",
             "-D__CUDA_NO_HALF2_OPERATORS__",
+            "--allow-unsupported-compiler"
         ]
     else:
         print("Compiling without CUDA")
